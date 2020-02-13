2d8232e298b656953ba8b2caf546d48aad31ecec
DMDDDM <41633121+DMDDDM@users.noreply.github.com>
2020-02-13 14:05:51 +0800

    This is runable app

--- app/src/main/java/com/dmdddm/ads/Collect.java
+++ app/src/main/java/com/dmdddm/ads/Collect.java
@@ -10,5 +10,6 @@ public class Collect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collect);
+        getSupportActionBar().hide();
     }
 }
--- app/src/main/java/com/dmdddm/ads/Location.java
+++ app/src/main/java/com/dmdddm/ads/Location.java
@@ -10,5 +10,6 @@ public class Location extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        getSupportActionBar().hide();
     }
 }
